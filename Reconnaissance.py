ab5650ecf4d8722cb1ba8266e2aa2f7947e67fa8
T05-17 <mahiraht99@gmail.com>
2020-02-27 11:14:23 +0800

    Mahirah

---
+++ Reconnaissance.py
@@ -0,0 +1,20 @@
+import requests
+url = 'http://172.17.50.43/algenius'
+r = requests.get(url)
+print(r.text)
+print('Status code:\n******')
+print(r.status_code)
+print("******")
+h = requests.head(url)
+print('Header:\n******')
+for line in h.headers:
+    print(line,":",h.headers[line])
+print("******")
+
+url2 = 'http://httpbin.org/headers'
+fake_headers = {
+    'User-Agent': 'Mobile'
+}
+r2 = requests.get(url2,headers = fake_headers)
+print(r2.text)
+
